3208295110ec268f36b0be7ab0cf35a95469cecd
mirfanz <dev.mirfan@gmail.com>
2025-11-26 01:48:00 +0700

    minor: change app icon

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "pemooda-cache-v1";
-const URLS_TO_CACHE = ["/", "/manifest.json", "/", "/favicon.ico"];
+const URLS_TO_CACHE = ["/", "/manifest.json", "/favicon.ico"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
